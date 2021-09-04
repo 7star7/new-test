5f0e3039c53d8e00dec7b0ed01f86f9262ce32d7
7star7 <81451588+7star7@users.noreply.github.com>
2021-09-04 12:45:07 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+int takeNum()
+{
+  int x{};
+  std::cout << "Please enter a number: ";
+  std::cin << x;
+  return x;
+}
+
 int main()
 {
   std::cout << "Welcome ♥ Hell\n";
+  int x{takeNum()};
+  int y{takeNum()};
+  std::cout << "Your answer is: " << x + y << '\n';
   return 0;
 }
